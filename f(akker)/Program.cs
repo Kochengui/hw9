70bea4b1ebcd9764a8e4b92c1d4b26687ee146b1
Anton <nachalnikkochengui@gmail.com>
2023-03-11 20:35:24 +0800

    ak

---
+++ f(akker)/Program.cs
@@ -0,0 +1,28 @@
+﻿Console.Write("Ввод числа M: ");
+int m = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Ввод числа N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+AkkerFunct(m,n);
+
+void AkkerFunct(int m, int n)
+{
+    Console.Write(Akker(m, n)); 
+}
+
+int Akker(int m, int n)
+{
+    if (m == 0)
+    {
+        return n + 1;
+    }
+    else if (n == 0 && m > 0)
+    {
+        return Akker(m - 1, 1);
+    }
+    else
+    {
+        return (Akker(m - 1, Akker(m, n - 1)));
+    }
+}
